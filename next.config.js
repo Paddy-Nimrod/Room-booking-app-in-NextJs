731364a369316a75af37214a0f987f09ae3291d3
unknown <n.ambetsa21@gmail.com>
2021-11-18 13:10:09 +0300

    added pagination, search and filter properties to the project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    MONGO_DB_URI:
+      "mongodb+srv://admin:ambetsa3270@main-booking-app.g2a1m.mongodb.net/BOOKIT-DB?retryWrites=true&w=majority"
+  }
+};
